ae6217e4fcfc87a1ed5b0edc7a2de13d5df5e350
user <del27-11@bk.ru>
2024-06-20 12:53:32 +0300

    task3 done

--- task3/task3.py
+++ task3/task3.py
@@ -1,17 +1,45 @@
-import sys,os
+import sys,os,json
 
 def check_file_exists(file_path):
     if not os.path.exists(file_path):
+        if "report.json" in file_path:
+            return True
         print(f"Файл '{file_path}' не существует.")
         return False
     return True
 
+def load_json(file_path):
+    with open(file_path,'r') as f:
+        return json.load(f)
+    
+def find_value(test_id, values):
+    for value in values:
+        if value['id'] == test_id:
+            return value['value']
+    return None
+
+def fill_values(tests, values):
+    for test in tests:
+        test_id = test['id']
+        value = find_value(test_id,values)
+        if value is not None:
+            test['value'] = value
+        if 'values' in test:
+            fill_values(test['values'], values)
+
+def save_json(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+        
 def create_report(path_values,path_tests,path_report):
-    pass
+    tests_data = load_json(path_tests)
+    values_data = (load_json(path_values))["values"]
+    fill_values(tests_data['tests'],values_data)
+    save_json(path_report, tests_data)
 
 def main():
     if len(sys.argv) != 4:
-        print("Использование: python3 task3.py <path1> <path2> <path3>")
+        print("Использование: python3 task3.py <path values.json> <path tests.json> <path report.json>")
         sys.exit(1)
 
     path_values = sys.argv[1]
@@ -25,7 +53,6 @@ def main():
             sys.exit(1)
 
     create_report(path_values,path_tests,path_report)
-
 
 if __name__ == "__main__":
     main()
